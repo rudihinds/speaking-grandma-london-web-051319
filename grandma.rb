71c1831038b71887e23827a844d88f1ba291dc82
rudi hinds <rudihinds@gmail.com>
2019-04-29 11:59:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else if  phrase != phrase.upcase
+  if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+      else if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+
   end
